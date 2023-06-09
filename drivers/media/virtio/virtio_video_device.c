aosp-mirror/kernel_common

CHROMIUM: drivers: media: virtio: disable ENUM_FRAMEINTERVALS ioctl for decoder
v4l2-compliance fails if it detects that VIDIOC_ENUM_FRAMEINTERVALS
ioctl is supported for a memory to memory device other then encoder.
Currently virtio-video allows calling VIDIOC_ENUM_FRAMEINTERVALS ioctl
for decoder devices.
In case of crosvm this call always returns EINVAL, because no
frame rates are provided by hypervisor for decoder device.

This patch disables this ioctl for virtio-video in order to comply
with v4l2-compliance.

Signed-off-by: Bartłomiej Grzesik <bag@semihalf.com>

BUG=b:160440787
TEST=v4l2-compliance -d /dev/video0
TEST=tast run eve arc.Video*

Change-Id: I708cc89f1eb98103692d78fdda3e14928fdb120c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3365197
Reviewed-by: David Staessens <dstaessens@chromium.org>
Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
Tested-by: Bartłomiej Grzesik <bgrzesik@google.com>
Commit-Queue: Marcin Wojtas <mwojtas@google.com>

drivers/media/virtio/virtio_video_device.c
@@ -1223,6 +1223,8 @@ virtio_video_device_create(struct virtio_video *vv)
 	v4l2_disable_ioctl(vd, VIDIOC_CROPCAP);
 	v4l2_disable_ioctl(vd, VIDIOC_G_CROP);
 	v4l2_disable_ioctl(vd, VIDIOC_S_CROP);
+	if (vv->vdev->id.device == VIRTIO_ID_VIDEO_DEC)
+		v4l2_disable_ioctl(vd, VIDIOC_ENUM_FRAMEINTERVALS);
 
 	video_set_drvdata(vd, vvd);
 

ago/linux

drivers/media/virtio/virtio_video_device.c
@@ -1223,6 +1223,8 @@ virtio_video_device_create(struct virtio_video *vv)
 	v4l2_disable_ioctl(vd, VIDIOC_CROPCAP);
 	v4l2_disable_ioctl(vd, VIDIOC_G_CROP);
 	v4l2_disable_ioctl(vd, VIDIOC_S_CROP);
+	if (vv->vdev->id.device == VIRTIO_ID_VIDEO_DECODER)
+		v4l2_disable_ioctl(vd, VIDIOC_ENUM_FRAMEINTERVALS);
 
 	video_set_drvdata(vd, vvd);
 